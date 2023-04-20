69a1d7a304516481077bcac84698d51cc228ae45
rosywijewardhanaa <rosywijewardhana00@gmail.com>
2023-04-20 16:51:07 +0530

    pytorch NLP DL chatbot added

---
+++ model.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+
+
+class NeuralNet(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(NeuralNet, self).__init__()
+        # self.l1 = nn.Linear(input_size, hidden_size) 
+        # self.l2 = nn.Linear(hidden_size, hidden_size) 
+        # self.l3 = nn.Linear(hidden_size, num_classes)
+        self.l1 = nn.Linear(input_size, 512) 
+        self.l2 = nn.Linear(512, 256) 
+        self.l3 = nn.Linear(256, 256)
+        self.l4 = nn.Linear(256, 128) 
+        self.l5 = nn.Linear(128, 64) 
+        self.l6 = nn.Linear(64, num_classes)
+        self.relu = nn.ReLU()
+    
+    def forward(self, x):
+        out = self.l1(x)
+        out = self.relu(out)
+        out = self.l2(out)
+        out = self.relu(out)
+        out = self.l3(out)
+        out = self.relu(out)
+        out = self.l4(out)
+        out = self.relu(out)
+        out = self.l5(out)
+        out = self.relu(out)
+        out = self.l6(out)
+        # no activation and no softmax at the end
+        return out
